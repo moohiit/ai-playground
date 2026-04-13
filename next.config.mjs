8d3270c74966006badd84c79edb0558bae770b79
Mohit Patel <pmohit645@gmail.com>
2026-04-13 23:16:20 +0530

    fix:build failed due to import using @

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,23 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     serverActions: { bodySizeLimit: "5mb" },
     serverComponentsExternalPackages: ["better-sqlite3"],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": __dirname,
+    };
+
+    return config;
   },
 };
 
